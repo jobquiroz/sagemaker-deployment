758358472c564407c450bac081419768aa6afadb
Job Quiroz <jobquiroz@hotmail.com>
2021-01-13 20:07:32 -0600

    Solution to project 1, sentiment analysis

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -69,10 +69,10 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
-
-    data_X = None
-    data_len = None
-
+    
+    data_X, data_len = convert_and_pad(model.word_dict, review_to_words(input_data))
+    
+    
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
     data_pack = np.hstack((data_len, data_X))
@@ -85,8 +85,10 @@ def predict_fn(input_data, model):
     model.eval()
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
-    #       be a numpy array which contains a single integer which is either 1 or 0
+    #       be a numpy array which contains a single integer which is either 1 or 0    
+    with torch.no_grad():
+        output = model.forward(data)
 
-    result = None
+    result = np.round(output.numpy())
 
     return result
